84c854790c9b936051a924a10851fcfe5a4bfb8b
Aiyada Agency <163337611+aiyadaagency@users.noreply.github.com>
2024-12-23 15:43:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -253,38 +253,3 @@ menu.addEventListener("click", () => {
     mobileDropdown.classList.remove("active");
   }
 });
-
-//
-
-let paragraphs = [...document.querySelectorAll(".reveal-text-effect")]
-let spans = [];
-
-paragraphs.forEach(paragraph => {
-    let htmlString = '';
-    let pArray = paragraph.textContent.split('');
-    for(let i = 0; i < pArray.length; i++) {
-        htmlString += `<span>${pArray[i]}</span>`;
-    }
-    paragraph.innerHTML = htmlString;
-});
-
-spans = [...document.querySelectorAll('.reveal-text-effect span')];
-
-function revealSpans() {
-    for(let i = 0; i < spans.length; i++) {
-     if(spans[i].parentElement.getBoundingClientRect().top < window.innerHeight / 2){
-        let {left, top} = spans[i].getBoundingClientRect();
-        top = top - (window.innerHeight * .2);
-        let opacityValue = 1 - ((top * .01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * 0.001)).toFixed(3);
-        opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
-        spans[i].style.opacity = opacityValue;
-     }
-    }
-}
-
-window.addEventListener('scroll', () => {
-    revealSpans();
-});
-
-revealSpans();
-
