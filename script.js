37fe8792907a126de21de071fc3eb206a9145bfd
Aiyada Agency <163337611+aiyadaagency@users.noreply.github.com>
2024-12-23 15:16:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -274,7 +274,7 @@ function revealSpans() {
     for(let i = 0; i < spans.length; i++) {
      if(spans[i].parentElement.getBoundingClientRect().top < window.innerHeight / 2){
         let {left, top} = spans[i].getBoundingClientRect();
-        top = top - (window.innerHeight * .5);
+        top = top - (window.innerHeight * .2);
         let opacityValue = 1 - ((top * .01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * 0.001));
         opacityValue = opacityValue > 1 ? 1 : opacityValue.toFixed(3);
         spans[i].style.opacity = opacityValue;
